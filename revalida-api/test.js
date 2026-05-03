21cdd18a35cb18b95c408105182692de51984f0c
Marco Simao <marco.a.simao@gmail.com>
2026-05-03 15:43:36 -0300

    Secret removido

--- revalida-api/test.js
+++ revalida-api/test.js
@@ -1,8 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "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",
-});
+  apiKey: ""});
 
 const response = openai.responses.create({
   model: "gpt-5.4-mini",
